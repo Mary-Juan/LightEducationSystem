c03910627106db0dcc603f37e8ed5042406ef1b2
mary <marya.6t@gmail.com@gmail.com>
2024-01-06 20:35:18 -0800

    I created layers and entities.

---
+++ LightEducationSystem.Model/Entities/TrainingCourse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightEducationSystem.Model.Entities
+{
+    public class TrainingCourse : BaseEntity
+    {
+        public string Title { get; set; }
+        public int Capacity { get; set; }
+        public string ProfessorId { get; set; }
+        public ICollection<TrainingCourseStudentCard> Cards { get; set; }
+        public TimeSpan Time { get; set; }
+    }
+}
---
+++ LightEducationSystem.Model/Entities/TrainingCourseStudentCard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightEducationSystem.Model.Entities
+{
+    public class TrainingCourseStudentCard 
+    {
+        public int StudentId { get; set; }
+        public int TrainingCourseId { get; set; }
+        public int Score { get; set; }
+    }
+}
