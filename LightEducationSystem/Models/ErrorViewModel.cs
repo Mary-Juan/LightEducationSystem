8e441a4a5aeefaedcb95095ce5b1c316aac22c1b
mary <marya.6t@gmail.com@gmail.com>
2024-01-06 20:29:50 -0800

    I want to implement a education system

---
+++ LightEducationSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LightEducationSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
